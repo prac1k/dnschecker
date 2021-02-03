3a7f87666fc2bdde3a9faf5a54ce407e9c34863c
prac1k <i.prac1k@gmail.com>
2021-02-03 18:04:10 +0200

    added whatsmy ip functionality

--- front/src/components/DcIpchk.vue
+++ front/src/components/DcIpchk.vue
@@ -1,10 +1,10 @@
 <template >
-  <div>
-    <h1>DC Ip Checker</h1>
+  <div class="ipchecker">
+    <h1>Ip Checker</h1>
   <div>
     <p>
      <input type="text" v-model="msg">
-     <button v-on:click="getipData">IP DC CHECK</button>
+     <button v-on:click="getipData">Check this IP!</button>
     </p>
   </div>
       <div v-for="ipData in ipDataList" :key="ipData.id">
@@ -17,6 +17,23 @@
           <p class="regionName">Region: {{ipDataList.regionName}}</p>
          </div>
         </div>
+  <div class="whatsmyip">
+    <h1>What's my Ip</h1>
+    <div>
+      <p>
+        <button v-on:click="getMyIpData">Check my IP!</button>
+      </p>
+    </div>
+    <div v-for="myIpData in myIpDataList" :key="myIpData.id">
+    </div>
+    <div>
+      <p class="query">Server IP address: {{myIpDataList.query}}</p>
+      <p class="isp"> ISP: {{myIpDataList.isp}}</p>
+      <p class="country">Country: {{myIpDataList.country}}</p>
+      <p class="city">City: {{myIpDataList.city}}</p>
+      <p class="regionName">Region: {{myIpDataList.regionName}}</p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -28,7 +45,9 @@ export default {
       ipDataList: [{
         regionName: null
       }],
-      msg: []
+      msg: [],
+      myIpDataList: [{
+      }],
     };
   },
   methods: {
@@ -37,6 +56,11 @@ export default {
     {
       axios.get("http://ip-api.com/json/"+this.msg
       ).then(response => (this.ipDataList = response.data));
+    },
+
+    getMyIpData()
+    {
+      axios.get("http://ip-api.com/json/").then(response => (this.myIpDataList = response.data));
 
 
     },
