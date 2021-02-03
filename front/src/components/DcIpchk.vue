61d9b19e4e634b8ec97587a586eb6d6dc9cdb1e0
prac1k <i.prac1k@gmail.com>
2021-02-03 08:44:11 +0200

    resolved duplicating entries

--- front/src/components/DcIpchk.vue
+++ front/src/components/DcIpchk.vue
@@ -1,32 +1,22 @@
-<template>
+<template >
   <div>
     <h1>DC Ip Checker</h1>
-    <div id="ip">
-        <p>
-          <input type="text" v-model="msg">
-          {{msg}}
-      <button v-on:click="getipData">IP DC CHECK</button>
-        </p>
-      </div>
-
-
-    <div v-for="ipData in ipDataList" :key="ipData.id" class="weather-data">
-      <div class="ipAddress">
-        <div>
-          <span>{{ipData.ipAddress}}</span>
-        </div>
-        <div>
-          <span class="continentName">{{ipData.continentName}}</span>
-        </div>
-      </div>
-      <div class="countryName">
-        <span>{{ipData.countryName}}°</span>
-      </div>
-      <div class="stateProv">
-        <span>{{ipData.stateProv}}°</span>
-      </div>
-    </div>
+  <div>
+    <p>
+     <input type="text" v-model="msg">
+     <button v-on:click="getipData">IP DC CHECK</button>
+    </p>
   </div>
+      <div v-for="ipData in ipDataList" :key="ipData.id">
+        </div>
+         <div>
+          <p class="query">Server IP address: {{ipDataList.query}}</p>
+           <p class="isp"> ISP: {{ipDataList.isp}}</p>
+          <p class="country">Country: {{ipDataList.country}}</p>
+          <p class="city">City: {{ipDataList.city}}</p>
+          <p class="regionName">Region: {{ipDataList.regionName}}</p>
+         </div>
+        </div>
 </template>
 
 <script>
@@ -35,15 +25,19 @@ export default {
   name: 'DcIpchk',
   data() {
     return {
-      ipDataList: [],
-      msg: null,
+      ipDataList: [{
+        regionName: null
+      }],
+      msg: []
     };
   },
   methods: {
-    getipData() {
-      axios.get("http://api.db-ip.com/v2/free/"+this.msg
+
+    getipData()
+    {
+      axios.get("http://ip-api.com/json/"+this.msg
       ).then(response => (this.ipDataList = response.data));
-      console.log(this.ipDataList)
+
 
     },
 
@@ -53,16 +47,5 @@ export default {
 
 <style scoped>
 
-.continentName {
-  flex-grow: 8;
-  text-align: center;
-  padding-left: 20px;
-}
 
-button {
-  padding:10px;
-  background-color: #1aa832;
-  color: white;
-  border: 1px solid #ccc;
-}
 </style>
