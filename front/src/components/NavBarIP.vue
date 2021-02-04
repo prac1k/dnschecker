47ddfb6c60d68f83041228b90573ba5f5a84a43f
prac1k <i.prac1k@gmail.com>
2021-02-04 15:59:35 +0200

    some UI work

--- front/src/components/NavBarIP.vue
+++ front/src/components/NavBarIP.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="navbar">
-    <div> <router-link to="/">Home</router-link> </div>
-    <div> <router-link to="/dcipchk">Ip checker</router-link> </div>
-    <div> <router-link to="/dnspropagation">Dns Propagation</router-link> </div>
+    <div class="navitem"> <router-link to="/">Home</router-link> </div>
+    <div class="navitem"> <router-link to="/dcipchk">Ip checker</router-link> </div>
+    <div class="navitem"> <router-link to="/dnspropagation">Dns Propagation</router-link> </div>
   </div>
 </template>
 
 <script>
-
+//import router from "../router/index.js"
 </script>
 
 <style scoped>
-
 .navbar {
-  text-align: center;
+  display: flex;
 }
+
+.navitem {
+  display: flex;
+  padding-right: 10px;
+  border-width: 1px;
+  border-style: solid;
+  border-color: black;
+}
+
 </style>
